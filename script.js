23ae5a196778dc65c85e1f85bee82405e506f4c8
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-02-17 12:51:32 +0100

    varie

--- script.js
+++ script.js
@@ -33,39 +33,58 @@
 /**** MODULES ****/
 
 /**** Default di Node js ****/
-const MyMath = require("./myMath.js"); // In questo modo creo un namespace.
+// const MyMath = require("./myMath.js"); // In questo modo creo un namespace.
 
-console.log(MyMath.sum(3, 4));
+// console.log(MyMath.sum(3, 4));
 
-console.log(MyMath.pow(3, 2));
+// console.log(MyMath.pow(3, 2));
 
-console.log(Math.pow(3, 2));
+// console.log(Math.pow(3, 2));
 
 
 
-const helper = require("./caseHelper.js");
+// const helper = require("./caseHelper.js");
 
-console.log(helper.splitCamelCase("laCasaRosa"));
+// console.log(helper.splitCamelCase("laCasaRosa"));
 
 /**** Modo più vecchio (prima di Node js) ****/
-function helper2() {
-    function startingUpperCase(string) {
-        return string[0].toUpperCase() + string.substring(1);
-    }
-    function splitCamelCase(string) {
-        let output = "";
-        for (const char of string) {
-            if (char === char.toUpperCase()) {
-                output += (" " + char.toLowerCase());
-            } else {
-                output += char;
-            }
-        }
-        return output;
-    }
-    return {startingUpperCase, splitCamelCase};
-}
+// function helper2() {
+//     function startingUpperCase(string) {
+//         return string[0].toUpperCase() + string.substring(1);
+//     }
+//     function splitCamelCase(string) {
+//         let output = "";
+//         for (const char of string) {
+//             if (char === char.toUpperCase()) {
+//                 output += (" " + char.toLowerCase());
+//             } else {
+//                 output += char;
+//             }
+//         }
+//         return output;
+//     }
+//     return {startingUpperCase, splitCamelCase};
+// }
 
-const h2 = helper2();
+// const h2 = helper2();
 
-console.log(h2.splitCamelCase("laCasaRosa"));
+// console.log(h2.splitCamelCase("laCasaRosa"));
+
+//-------------------------------------------------//
+
+/**** Modulo esterno ****/
+
+const csv = require('csv-parser')
+const fs = require('fs')
+const results = [];
+
+fs.createReadStream('data.csv')
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+        console.log(results);
+        // [
+        //   { NAME: 'Daffy Duck', AGE: '24' },
+        //   { NAME: 'Bugs Bunny', AGE: '22' }
+        // ]
+    });
